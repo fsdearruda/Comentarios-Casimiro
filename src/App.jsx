fb743718b89ddb0f8052c1472b20c85cdcc50ab0
Fernando Arruda <fernandojs006@gmail.com>
2022-02-13 00:37:55 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./app.css";
+import adjetivos from "./components/adjetivos";
+import Comment from "./components/Comment";
+
+const gerarAdjetivo = () => {
+  return adjetivos[Math.floor(Math.random() * adjetivos.length)];
+};
+
+const App = () => {
+  const [comments, setComments] = useState([]);
+  const [isLoaded, setLoaded] = useState(false);
+  useEffect(() => {
+    if (isLoaded) return;
+    axios
+      .get("https://randomuser.me/api/?results=10&gender=male")
+      .then(response => {
+        let users = [];
+        response.data.results.forEach(user => {
+          user = {
+            username: user.login.username,
+            profilePic: user.picture.medium,
+            commentLikes: Math.floor(Math.random() * 200) + 1,
+            message: `Simplesmente o ${gerarAdjetivo()} do entretenimento`,
+            createdAt: `há ${Math.floor(Math.random() * 23) + 1} horas atrás`
+          };
+          users.push(user);
+        });
+        setComments(users);
+        setLoaded(true);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [comments, isLoaded]);
+
+  return (
+    <div>
+      {comments.sort((a,b) => b.commentLikes-a.commentLikes).map(user => (
+        <Comment key={comments.indexOf(user)} info={user} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Comment.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "./comment.css";
+import { MdThumbUp, MdOutlineThumbUp, MdThumbDown, MdOutlineThumbDown } from "react-icons/md";
+
+const Comment = props => {
+  const { profilePic, username, commentLikes, message, createdAt } = props.info;
+  const [isLiked, setLiked] = useState(false);
+  const [isDisliked, setDisliked] = useState(false);
+  const [likes, setLikes] = useState(commentLikes);
+
+  const handleLike = liked => {
+    if (liked) {
+      setLikes(likes - 1);
+      setLiked(false);
+      setDisliked(false);
+    } else {
+      setLikes(likes + 1);
+      setLiked(true);
+      setDisliked(false);
+    }
+  };
+  const handleDislike = disliked => {
+    if (disliked) {
+      setLiked(false);
+      setDisliked(false);
+    } else {
+      setDisliked(true);
+      setLiked(false);
+      if (isLiked) setLikes(likes - 1);
+    }
+  };
+
+  return (
+    <div className="comment">
+      <img className="author-picture" src={profilePic} alt="" />
+      <div className="comment-details">
+        <div>
+          <span className="comment-author">{username} </span>
+          <span className="comment-date">{createdAt}</span>
+        </div>
+        <span>{message}</span>
+        <div className="comment-likes">
+          <span className="like-btn">{!isLiked ? <MdOutlineThumbUp onClick={() => handleLike(isLiked)} /> : <MdThumbUp onClick={() => handleLike(isLiked)} />}</span>
+          <span className="like-qnt">{likes}</span>
+          <span className="like-btn">
+            {!isDisliked ? <MdOutlineThumbDown onClick={() => handleDislike(isDisliked)} /> : <MdThumbDown onClick={() => handleDislike(isDisliked)} />}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Comment;
